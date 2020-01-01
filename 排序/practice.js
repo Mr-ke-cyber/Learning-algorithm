b802cca45f6e564c7dcf3e9697f9c070194d3dcf
keon <863259871@qq.com>
2020-01-01 22:30:33 +0800

    练习冒泡算法和快速排序

--- 排序/practice.js
+++ 排序/practice.js
@@ -1,21 +1,18 @@
 //练习本专题下的算法题
-
+let count = 0;
 function bubbleSort(array) {
-    for (let j = 0; j < array.length; j++) {
+    for (let i = 0; i < array.length; i++) {
         let complete = true;
-        for (let i = 0; i < array.length - 1 - j; i++) {
-            // 比较相邻数
-            if (array[i] > array[i + 1]) {
-                [array[i], array[i + 1]] = [array[i + 1], array[i]];
+        for (let j = i + 1; j < array.length; j++) {
+            count++;
+            if ( array[i] > array[j]) {
                 complete = false;
+                [array[i], array[j]] = [array[j], array[i]];
             }
         }
-        // 没有冒泡结束循环
-        if (complete) {
-            break;
-        }
+        if (complete) break;
     }
-    return array;
+    return array
 }
 let result = bubbleSort([1, 2, 4, 6, 9, 3, 8]);
-console.log(result);
+console.log(result, count);
