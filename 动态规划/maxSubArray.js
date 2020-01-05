6bc9ca8d56c815228087c2af0d23d5d1e730a4f1
keon <863259871@qq.com>
2020-01-05 23:29:59 +0800

    feat:动态规划解决最大子序列和的问题

---
+++ 动态规划/maxSubArray.js
@@ -0,0 +1,30 @@
+// 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+//
+// 示例:
+//
+//     输入: [2,1,-3,4,-1,2,1,-5,4],
+//         输出: 6
+// 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+// 进阶:
+//
+//     如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+
+var maxSubArray = function(nums) {
+    let len = nums.length;
+    let dp = Array(len).fill(0);
+    let maxSum = nums[0];
+    dp[0] = nums[0];
+    for (let i = 1; i < len; i++) {
+        dp[i] = Math.max(nums[i], dp[i - 1] + nums[i]);
+        if (dp[i] > maxSum) {
+            maxSum = dp[i];
+        }
+    }
+    return maxSum;
+};
+let result = maxSubArray([-3,4,-1,2,1,-5,4]);
+console.log(result);
+
+
+
+
