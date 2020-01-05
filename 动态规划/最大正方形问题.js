a7abd50081bf4aa0a3d4e81973b4e740df2d35c1
keon <863259871@qq.com>
2020-01-05 21:21:39 +0800

    feat:动态规划解答最大正方形问题

--- 动态规划/最大正方形问题.js
+++ 动态规划/最大正方形问题.js
@@ -11,37 +11,46 @@
 var maximalSquare = function(matrix) {
     if (matrix.length === 0) return 0;
     let row = matrix.length;
-    let col = matrix[0].length;
+    let col = matrix[0] ? matrix[0].length : 0;
+    let ans = 0;
     let dp = Array(row).fill('-').map( _ => Array(col).fill(0));
-    let maxSquare = 0;
-    for (let i = 1; i < row; i++) {
+    for (let i = 0; i < row; i++) {
         dp[i][0] = matrix[i][0];
+        if(dp[i][0] == 1) {
+            ans = 1;
+        }
+    }
+    for (let j = 0; j < col; j++) {
+        dp[0][j] = matrix[0][j];
+        if (dp[0][j] == 1) {
+            ans = 1;
+        }
+    }
+    for (let i = 1; i < row; i++) {
         for (let j = 1; j < col; j++) {
-            dp[0][j] = matrix[0][j];
             if (matrix[i][j] == 1) {
                 if (matrix[i - 1][j - 1] == 0) {
                     dp[i][j] = 1;
                 } else {
                     let x = i;
                     let y = j;
-                    while (x > 0 && matrix[x][j] == 1) {
+                    while (x >= 0 && matrix[x][j] == 1) {
                         x--;
                     }
-                    while (y > 0 && matrix[i][y] == 1) {
+                    while (y >= 0 && matrix[i][y] == 1) {
                         y--;
                     }
-                    let maxBian = Math.min(i - x , j - y);
-                    let newSquare = maxBian * maxBian;
-                    if (newSquare > maxSquare) {
-                        maxSquare = newSquare;
-                    }
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1;
                 }
+                ans = Math.max(dp[i][j], ans)
             }
         }
     }
-    return maxSquare;
+    return ans * ans;
 };
 let result = maximalSquare(
-     [[0,1]]
+     [[1,0,1],
+            [1,1,1],
+            [1,1,1]]
 );
 console.log(result);
