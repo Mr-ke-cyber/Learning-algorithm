fdada4bbdf704862d9ab29f434310f7955f330b5
keon <863259871@qq.com>
2020-02-05 22:09:47 +0800

    feat:双指针加递归求最小回文字串

--- 挑战字符串/shortestPalindrome-lt214.js
+++ 挑战字符串/shortestPalindrome-lt214.js
@@ -11,7 +11,8 @@
  */
 /*初一看到这个题目，思路是先找出字串中已经存在的最长回文串，再将这个回文串添加字符扩展至整个字串，那么它就是找到的转换的最短回文串了*/
 /*注意读题，题意是在字符串前面添加字串，不用考虑在字符串后面添加字串的情况 刚开始做成了在首尾都可以增加字母的情况*/
-var shortestPalindrome = function(s) {
+/*解法一 暴力法*/
+var shortestPalindrome = function (s) {
     let len = s.length;
     let rev = s.split("").reverse().join("");
     for (let i = 0; i < len; i++) {
@@ -21,6 +22,21 @@ var shortestPalindrome = function(s) {
     }
     return ""
 };
+/*解法二：双指针法与递归法*/
+var shortestPalindrome2 = function (s) {
+    let len = s.length;
+    let l = 0;
+    for (let r = len - 1; r >= 0; r--) {
+        if (s.charAt(r) === s.charAt(l)) {
+            l++;
+        }
+    }
+    if (l === len) {
+        return s;
+    }
+    let remain_rev = s.slice(l, len).split("").reverse().join("");
+    return remain_rev + shortestPalindrome2(s.slice(0, l)) + s.slice(l);  // 此处递归较难想到，很有可能直接返回了，关键点在于0 - l字串不一定就是回文子串
+};
 
-let result = shortestPalindrome("aacecaaa");
+let result = shortestPalindrome2("aababa");
 console.log(result);
