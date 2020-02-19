9f35606682a7296ddc58d3b90356b38106c30a22
keon <863259871@qq.com>
2020-02-19 13:54:26 +0800

    feat:双指针解决乘积小于K的子数组个数

---
+++ 双指针/numSubarrayProductLessThanK-lt713-medium.js
@@ -0,0 +1,33 @@
+// 给定一个正整数数组 nums。
+// 找出该数组内乘积小于 k 的连续的子数组的个数。
+// 示例 1:
+// 输入: nums = [10,5,2,6], k = 100
+// 输出: 8
+// 解释: 8个乘积小于100的子数组分别为: [10], [5], [2], [6], [10,5], [5,2], [2,6], [5,2,6]。
+// 需要注意的是 [10,5,2] 并不是乘积小于100的子数组。
+// 说明:
+//     0 < nums.length <= 50000
+// 0 < nums[i] < 1000
+// 0 <= k < 10^6
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var numSubarrayProductLessThanK = function(nums, k) {
+    if (k <= 1) return 0;
+    let len = nums.length;
+    let res = 0;
+    let prod = 1;
+    let l = 0;
+    for (let r = 0; r < len; r++) {
+        prod *= nums[r];
+        while (prod >= k) {
+            prod /= nums[l++];
+        }
+        res += r - l + 1;
+    }
+    return res;
+};
+let result = numSubarrayProductLessThanK([10,5,2,6], 100);
+console.log(result);
