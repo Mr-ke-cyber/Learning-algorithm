362d1a17586cf5491fa27b1d65b759c4fa76edbb
keon <863259871@qq.com>
2020-02-24 21:50:10 +0800

    feat:复习选择排序

--- 排序/practice.js
+++ 排序/practice.js
@@ -1,27 +1,15 @@
-var bubbleSort = function (array) {
+var selectSort = function (array) {
     let len = array.length;
     for (let i = 0; i < len; i++) {
+        let maxIndex = i;
         for (let j = i + 1; j < len; j++) {
-            if (array[i] > array[j]) {
-                [array[i], array[j]] = [array[j], array[i]];
+            if (array[j] > array[maxIndex]) {
+                maxIndex = j;
             }
         }
+        [array[i], array[maxIndex]] = [array[maxIndex], array[i]];
     }
     return array;
 };
-var bubbleSort2 = function (array) {
-    let len = array.length;
-    for (let i = 0; i < len; i++) {
-        let complete = true;
-        for (let j = 0; j < len - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                [array[j], array[j + 1]] = [array[j + 1], array[j]];
-                complete = false;
-            }
-        }
-        if (complete) break;
-    }
-    return array;
-};
-let result = bubbleSort2([3,1,5,9,2,1,0]);
+let result = selectSort([3, 1, 4, 6, 9, 2, 8]);
 console.log(result);
