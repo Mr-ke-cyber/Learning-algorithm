e43261c0bec151828840cdbdde7791ca9d71a4fb
keon <863259871@qq.com>
2020-02-28 11:55:57 +0800

    feat:复习归并排序

--- 排序/practice.js
+++ 排序/practice.js
@@ -1,17 +1,27 @@
-var quickSort = function (array) {
+let mergeSort = function (array) {
     let len = array.length;
     if (len < 2) return array;
-    let left = [];
-    let right = [];
-    let target = array[0];
-    for (let i = 1; i < len; i++) {
-        if (array[i] < target) {
-            left.push(array[i]);
+    let mid = len >> 1;
+    let left = array.slice(0, mid);
+    let right = array.slice(mid);
+    return merge(mergeSort(left), mergeSort(right));
+};
+let merge = function (left, right) {
+    let temp = [];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            temp.push(left.shift());
         } else {
-            right .push(array[i]);
+            temp.push(right.shift());
         }
     }
-    return quickSort(left).concat(target, quickSort(right));
+    while (left.length) {
+        temp.push(left.shift());
+    }
+    while (right.length) {
+        temp.push(right.shift());
+    }
+    return temp;
 };
-let result = quickSort([3, 1, 4, 6, 9, 2, 8]);
+let result = mergeSort([3, 1, 4, 6, 9, 2, 8]);
 console.log(result);
