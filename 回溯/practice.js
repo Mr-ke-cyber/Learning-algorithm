f87799f880547a83120d4cf32322e0da7fd8e442
keon <863259871@qq.com>
2020-11-17 10:18:49 +0800

    feat: 不同的二叉搜索树

--- 回溯/practice.js
+++ 回溯/practice.js
@@ -6,20 +6,27 @@ var permute = function(nums) {
     let len = nums.length;
     let tempPath = [];
     let res = [];
+    let used = new Array(len).fill(false);
+    nums.sort((a, b) => a - b);
     const backTrace = (tempPath) => {
         if(tempPath.length === len) {
             res.push(tempPath.slice());
         }
         for(let i = 0; i < nums.length; i++) {
-            if (!tempPath.includes(nums[i])) {
+            if (i > 0 && nums[i] === nums[i - 1] && used[i - 1] === false) {
+                continue;
+            }
+            if (!used[i]) {
                 tempPath.push(nums[i]);
+                used[i] = true;
                 backTrace(tempPath);
                 tempPath.pop();
+                used[i] = false;
             }
         }
     };
     backTrace(tempPath);
     return res;
 };
-let result = permute([1,2,3]);
+let result = permute([1,1,2]);
 console.log(result, 'jk')
--- 排序/practice.js
+++ 排序/practice.js
@@ -1,8 +1,20 @@
-const a = {b: 3};
-function foo (obj) {
-    obj.b = 5;
-    return obj;
-}
-const aa = foo (a);
-console.log(a.b);  // 5
-console.log(aa.b); // 5
+/**
+ * @param {number} R
+ * @param {number} C
+ * @param {number} r0
+ * @param {number} c0
+ * @return {number[][]}
+ */
+var allCellsDistOrder = function(R, C, r0, c0) {
+    let res = [];
+    for(let i = 0; i < R; i++) {
+        for(let j = 0; j < C; j++) {
+            res.push([i, j]);
+        }
+    }
+    return res.sort((a, b) => {
+        return Math.abs(a[0] - r0) + Math.abs(a[1] - c0) - Math.abs(b[0] - r0) - Math.abs(b[1] - c0);
+    })
+};
+let result = allCellsDistOrder(2, 2, 0, 1);
+console.log(result);
